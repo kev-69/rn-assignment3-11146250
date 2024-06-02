7a41d52ad91c0c00d875eaa07c81acc16ba9c5c1
kev-69 <bismarkobuobi19@gmail.com>
2024-06-02 13:41:52 +0000

    completed greetings and the search ocntainer at the top of the ui

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <SafeAreaView>
+    <View style={styles.safeArea}>
+      <View style={styles.mainContainer}>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.helloText}>Hello, Devs</Text>
+            <Text style={styles.taskText}>14 tasks today</Text>
+          </View>
+
+          <TouchableOpacity style={styles.profileBtn}>
+            <Image source={require("./assets/person.png")} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.searchContainer}>
+          <TextInput
+            placeholder='Search'
+            style={styles.textInput}
+          />
+          <View style={styles.filterBtn}>
+            <Image source={require("./assets/bx_slider.png")} />
+          </View>
+        </View>
+
+        <View>
+          
+        </View>
+      </View>
     </View>
+    // </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f7f0e8',
+    padding: 12,
+    gap: 20,
   },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  helloText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  taskText: {
+    fontSize: 14,
+    fontWeight: "normal",
+  },
+  profileBtn: {
+    backgroundColor: "#fff",
+    borderRadius: 50,
+    padding: 8,
+  },
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    top: 10
+  },
+  textInput: {
+    backgroundColor: "#fff",
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    padding: 5,
+  },
+  filterBtn: {
+    backgroundColor: "#f0522f",
+    padding: 8,
+    borderRadius: 10,
+  },
+  categoriesContainer: {
+    gap: 10,
+  },
+  categoriesTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: 1.25,
+  }
 });
