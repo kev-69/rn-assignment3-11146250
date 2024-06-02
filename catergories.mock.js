14d5b3dc43414ba8a6171f1d8ccd51663150a0a4
kev-69 <bismarkobuobi19@gmail.com>
2024-06-02 14:14:38 +0000

    Mock data to be used in ongoing tasks section

--- catergories.mock.js
+++ catergories.mock.js
@@ -3,18 +3,18 @@ export const categoriesData = [
         id: 0,
         name: "Exercise",
         description: "12 Tasks",
-        img: require("./assets/young-woman working-online.png"),
+        img: require("./assets/young-woman-working-online.png"),
     },
     {
         id: 1,
         name: "Study",
         description: "12 Tasks",
-        img: require("./assets/young-woman working-at-desk.png"),
+        img: require("./assets/young-woman-working-at-desk.png"),
     },
     {
         id: 2,
         name: "Exercise",
         description: "12 Tasks",
-        img: require("./assets/young-woman working-online.png"),
+        img: require("./assets/young-woman-working-online.png"),
     }
 ]
