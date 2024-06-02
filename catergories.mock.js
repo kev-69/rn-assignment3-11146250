3c32ab6fcfe7559b53e27395c7a4a152f90cb092
kev-69 <bismarkobuobi19@gmail.com>
2024-06-02 13:53:59 +0000

    Categories section data file updated

---
+++ catergories.mock.js
@@ -0,0 +1,20 @@
+export const categoriesData = [
+    {
+        id: 0,
+        name: "Exercise",
+        description: "12 Tasks",
+        img: require("./assets/young-woman working-online.png"),
+    },
+    {
+        id: 1,
+        name: "Study",
+        description: "12 Tasks",
+        img: require("./assets/young-woman working-at-desk.png"),
+    },
+    {
+        id: 2,
+        name: "Exercise",
+        description: "12 Tasks",
+        img: require("./assets/young-woman working-online.png"),
+    }
+]
